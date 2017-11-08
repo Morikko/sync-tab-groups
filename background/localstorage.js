cf9fe20c5ccd814b5d2016940eb921f878812819
Eric Masseran <rico.masseran@gmail.com>
2017-11-08 18:45:58 +0100

    Interface of the use of local storage

---
+++ background/localstorage.js
@@ -0,0 +1,26 @@
+var StorageManager = StorageManager || {};
+
+var StorageManager.Local = StorageManager.Local || {};
+
+/**
+ * Save the groups as JSON object in the local storage (this computer, this session)
+ * @param {Array[Group]} groups
+ */
+StorageManager.Local.saveGroups( groups ) {
+
+}
+
+/**
+ * Load the groups from the local storage (this computer, this session) and return it as a javascript object
+ * @return {Array[Group]} groups
+ */
+StorageManager.Local.loadGroups( ) {
+
+}
+
+/**
+ * Clean the local storage (this computer, this session, this extension)
+ */
+StorageManager.Local.cleanGroups( ) {
+
+}
