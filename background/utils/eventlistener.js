3f6d04715cb23c123a0f7cecde2dd74952096f56
Eric Masseran <rico.masseran@gmail.com>
2017-11-18 16:58:02 +0100

    Add event listener class

---
+++ background/utils/eventlistener.js
@@ -0,0 +1,16 @@
+EventListener = function() {
+ this.events = []; // Empty list of events/actions
+}
+
+EventListener.prototype.on = function(event, fn) {
+  this.events[event] = this.events[event] || [];
+  this.events[event].push(fn);
+}
+
+EventListener.prototype.fire = function(event) {
+  if (this.events[event]) {
+    this.events[event].forEach(function(fn) {
+      fn();
+    })
+  }
+}
