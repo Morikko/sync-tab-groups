bb4e7d4244999ab82e13e6816f34c897ad1232be
Eric Masseran <rico.masseran@gmail.com>
2017-11-27 13:10:27 +0100

    FileStorage: interface

--- background/storage/filestorage.js
+++ background/storage/filestorage.js
@@ -1,8 +1,39 @@
 /**
  * Everything related to save file on the disk (Not through firefox API)
  */
- var StorageManager = StorageManager || {};
+var StorageManager = StorageManager || {};
 
- StorageManager.File = StorageManager.File || {};
+StorageManager.File = StorageManager.File || {};
 
- 
+StorageManager.File.exportGroups = function(file, groups ){
+
+}
+
+StorageManager.File.importGroups = function(file){
+  let groups = [];
+
+  return groups;
+}
+
+StorageManager.File.getFile = function () {
+  let file = "";
+
+  return file;
+}
+
+
+StorageManager.File.importSyncTabGroups = function( data_imported ){
+  let groups = [];
+
+  return groups;
+}
+
+/**
+ *
+ * legacy
+ */
+StorageManager.File.importTabGroups = function( data_imported ){
+  let groups = [];
+
+  return groups;
+}
