024b49b0ea68e5b205284ce46d314c1f278cdb26
Eric Masseran <rico.masseran@gmail.com>
2017-11-27 13:04:34 +0100

    Add filestorage file

---
+++ background/storage/filestorage.js
@@ -0,0 +1,8 @@
+/**
+ * Everything related to save file on the disk (Not through firefox API)
+ */
+ var StorageManager = StorageManager || {};
+
+ StorageManager.File = StorageManager.File || {};
+
+ 
