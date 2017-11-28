9f7488028fcf393769c1ace3cd95c425ac1150c2
Eric Masseran <rico.masseran@gmail.com>
2017-11-28 15:39:46 +0100

    Import/Export add notes in option

--- option-page/components/file_section.js
+++ option-page/components/file_section.js
@@ -17,23 +17,9 @@ const FileSection = React.createClass({
       }),
       React.DOM.span({},
         React.DOM.ul({}, [
-          React.DOM.li({}, "Bllooooo"),
+          React.DOM.li({}, "Imported groups are added as closed groups"),
+          React.DOM.li({}, "Import supports: Sync Tab Groups and Tab Groups json file."),
         ])),
-      /*
-      React.createElement(OptionInput, {
-        label: "Name of the session: ",
-        help: "Example: Computer 1, laptop...",
-        name: this.props.options.folder,
-        onChange: this.props.onOptionChange,
-        id: this.prefix + "-folder",
-      }),
-      React.createElement(NiceCheckbox, {
-        checked: this.props.options.sync,
-        label: "Save groups in bookmarks automatically (Every 30s)",
-        onCheckChange: this.props.onOptionChange,
-        id: this.prefix + "-sync",
-      }),
-      */
       React.createElement(OptionButton, {
         title: "Export Groups",
         onClick: this.props.onExportClick
