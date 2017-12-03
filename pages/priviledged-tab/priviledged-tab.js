7224127b11f1a7c102c89951fa4ce920334e36fb
Eric Masseran <rico.masseran@gmail.com>
2017-12-03 19:56:11 +0100

    Add test page for priviledged URLs

---
+++ pages/priviledged-tab/priviledged-tab.js
@@ -0,0 +1,15 @@
+function getParameterByName(name, url) {
+  if (!url) url = window.location.href;
+  name = name.replace(/[\[\]]/g, "\\$&");
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+    results = regex.exec(url);
+  if (!results) return null;
+  if (!results[2]) return '';
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.title = getParameterByName('title').replace('+', ' ');
+  let display_url = document.getElementById("display_url");
+  display_url.innerHTML = getParameterByName('url');
+});
