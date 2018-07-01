bdaff3e37587bf02cbdfa3815605c2a1033f5b65
Eric Masseran <rico.masseran@gmail.com>
2018-07-01 17:44:55 +0200

    Remove experimental message for hidden API

--- extension/optionpages/components/section_settings.js
+++ extension/optionpages/components/section_settings.js
@@ -310,23 +310,6 @@ class SettingsSection extends React.Component {
         null,
         React.createElement(
           "div",
-          { className: "experimental" },
-          "All the features related to the hidden tabs are experimental. Please refer to the ",
-          React.createElement(
-            "a",
-            { href: "https://github.com/Morikko/sync-tab-groups/wiki/Hiding-support" },
-            "manual page"
-          ),
-          " to know how to use them. Note that the features might not work as you expect, take your precaution. The hiding support is only provided by Firefox as an experimental feature. You need to activate the hiding functionality in theee Firefox settings. For any issue, ",
-          React.createElement(
-            "a",
-            { href: "https://github.com/Morikko/sync-tab-groups/issues/64" },
-            "please report here"
-          ),
-          "."
-        ),
-        React.createElement(
-          "div",
           { className: "double-buttons" },
           React.createElement(OptionButton, {
             title: "Close",
--- extension/optionpages/components/section_settings.jsx
+++ extension/optionpages/components/section_settings.jsx
@@ -180,9 +180,6 @@ class SettingsSection extends React.Component {
         }
         content={
           <div>
-            <div className="experimental">
-              All the features related to the hidden tabs are experimental. Please refer to the <a href="https://github.com/Morikko/sync-tab-groups/wiki/Hiding-support">manual page</a> to know how to use them. Note that the features might not work as you expect, take your precaution. The hiding support is only provided by Firefox as an experimental feature. You need to activate the hiding functionality in theee Firefox settings. For any issue, <a href="https://github.com/Morikko/sync-tab-groups/issues/64">please report here</a>.
-            </div>
             <div className="double-buttons">
               <OptionButton
                 title= {"Close"}
