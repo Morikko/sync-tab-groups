28bb20746e083ae61728f0d0897a7eb8999be716
Eric Masseran <rico.masseran@gmail.com>
2018-11-05 00:04:56 +0100

    Add zip

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const cssnano = require('cssnano');
 
